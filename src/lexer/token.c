ded76b3ec88ae17586d7e9b5a927bc8c89cb00ee
0xRuFFy <konstantinopora@gmail.com>
2024-05-09 22:50:28 +0200

    no capslock

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -2,7 +2,7 @@
 
 #include "lexer/token.h"
 
-char* const TOKEN_TYPE_STRINGS[] = {
+char* const token_type_strings[] = {
     "EOF",
     "INVALID",
 
@@ -32,13 +32,13 @@ char* const TOKEN_TYPE_STRINGS[] = {
     "FLOAT",
 };
 
-ASSERT_ENUM_ELEMENT_COUNT(TOKEN_TYPE_STRINGS, TT_MAX);
+ASSERT_ENUM_ELEMENT_COUNT(token_type_strings, TT_MAX);
 
-char* token_type_to_string(TokenType token) {
-    return TOKEN_TYPE_STRINGS[token];
+char* token_type_to_string(const TokenType token) {
+    return token_type_strings[token];
 }
 
-void print_token(Token* token) {
+void print_token(const Token* token) {
     printf("[%-15s] ~ %-60.*s ", token_type_to_string(token->type), (int)token->value_len, token->value);
     print_location(token->location);
 }
