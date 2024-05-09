082f917db811810ab0b2770c61c0c28328c73b46
0xRuFFy <konstantinopora@gmail.com>
2024-05-09 22:48:45 +0200

    added const to function param

--- src/lexer/location.c
+++ src/lexer/location.c
@@ -2,6 +2,6 @@
 
 #include "lexer/location.h"
 
-void print_location(Location location) {
+void print_location(const Location location) {
     printf("<%40s> - (%7lu:%4lu)\n", location.file_path, location.line, location.column);
 }
